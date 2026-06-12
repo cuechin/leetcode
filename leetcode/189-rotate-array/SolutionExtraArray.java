15d3cf40b75aed156fb1c7df27ba08fb8f5fd3af
andrés <andres_1193@hotmail.com>
2026-06-12 14:23:12 -0600

    Create SolutionExtraArray.java

---
+++ leetcode/189-rotate-array/SolutionExtraArray.java
@@ -0,0 +1,17 @@
+public class SolutionExtraArray {
+
+    public void rotate(int[] nums, int k) {
+        
+    }
+
+    public static void main(String[] args) {
+
+        SolutionExtraArray solution = new SolutionExtraArray();
+
+        int[] nums = { 1,2,3,4,5,6,7 };
+        int k = 3;
+        solution.rotate(nums, k);
+        System.out.println();
+    }
+    
+}
